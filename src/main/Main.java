1a04742b60b5e6fbe2b389006021acb666e9b34f
Luka <luka.brcic@tp.rs>
2022-03-08 19:26:31 +0100

    Initial commit

---
+++ src/main/Main.java
@@ -0,0 +1,52 @@
+package main;
+
+import java.time.Clock;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Main {
+
+    public static AtomicInteger sum = new AtomicInteger(0);
+    public static AtomicInteger numb = new AtomicInteger(0);
+    public static BlockingDeque<Student> queue = new LinkedBlockingDeque<>();
+    public static List<Student> studentsList = new ArrayList<>();
+    public static CyclicBarrier cyclicBarrier = new CyclicBarrier(2);
+
+    public static void main(String[] args) {
+
+        //init students
+        System.out.println("Uneti broj studenata: ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i = 0; i < n; i++){
+            Student s = new Student(i);
+            studentsList.add(s);
+        }
+
+        Collections.sort(studentsList);
+
+        for (int i = 0; i < n; i++){
+            queue.add(studentsList.get(i));
+        }
+
+        //Threads init
+        ExecutorService pool = Executors.newFixedThreadPool(3);
+        pool.submit(new Profesor(1));
+        pool.submit(new Profesor(2));
+        pool.submit(new Asistent(1));
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        pool.shutdownNow();
+        System.out.println(sum.get() * 1.0 / numb.get());
+
+    }
+}
---
+++ src/main/Profesor.java
@@ -0,0 +1,43 @@
+package main;
+
+import java.util.Random;
+import java.util.concurrent.BrokenBarrierException;
+
+public class Profesor implements Runnable{
+
+    private int id;
+
+    public Profesor(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public void run() {
+        while(true){
+            try {
+                Student s = Main.queue.take();
+                if(s.getArrival() - System.currentTimeMillis() > 0)
+                    Thread.sleep(s.getArrival() - System.currentTimeMillis());
+
+                Main.cyclicBarrier.await();
+                long pocetakOdbrane = System.currentTimeMillis();
+
+                int grade = new Random().nextInt(5) + 5;
+                System.out.println(
+                        " Thread: " + s.getId() +
+                        " Arrival: " + s.getArrival() +
+                        " Profesor: " + this.id +
+                        " TTC: " + s.getLen() + " : " + pocetakOdbrane +
+                        " Score: " + grade
+                );
+                Thread.sleep(s.getLen());
+                Main.sum.addAndGet(grade);
+                Main.numb.addAndGet(1);
+            } catch (InterruptedException | BrokenBarrierException e) {
+                e.printStackTrace();
+                break;
+            }
+
+        }
+    }
+}
